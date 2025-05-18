620ff8456870c2e10c54174631761a92f1b91314
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2025-05-18 18:42:52 -0300

    feat: implement home page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <main>Home</main>
+import style from './HomePage.module.css'
+
+export default function HomePage() {
+  return (
+    <div className={ style.home }>
+      <header className={ style.header }>
+        <h2>Salão das Histórias</h2>
+      </header>
+
+      <div className={ style.storiesList }>
+        <div className={style.card }>
+          <div className={ style.cardImage }>+</div>
+        </div>
+
+        <div className={ style.card }>
+          <div className={ style.cardImage }></div>
+          <div className={ style.storyDetailAndOptions }>
+            <h4>História A</h4>
+            <div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
 }
