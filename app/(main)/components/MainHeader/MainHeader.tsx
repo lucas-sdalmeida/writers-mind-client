ca8b9f84a283fc2b4d97a0cae692e1d71f4615dc
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2025-05-22 08:50:31 -0300

    refactor: use tailwind in MainHeader

--- app/(main)/components/MainHeader/MainHeader.tsx
+++ app/(main)/components/MainHeader/MainHeader.tsx
@@ -1,13 +1,12 @@
 import { Mr_Dafoe } from 'next/font/google'
 import AuthorInfo, { Orientation } from '../AuthorInfo'
-import style from './Header.module.css'
 
 const mrDafoe = Mr_Dafoe({ weight: '400', subsets: ['latin'] })
 
 export default function MainHeader({ className, pageTitle }: Props) {
   return (
-    <header className={` ${className} ${style.header}`}>
-      <h2 className={mrDafoe.className}>Writer&apos;s Mind</h2>
+    <header className={`${className} w-full h-12 py-1 px-4 shadow-md shadow-gray-400 bg-[#f6f6f6] flex justify-between items-center`}>
+      <h2 className={`${mrDafoe.className} text-xl`}>Writer&apos;s Mind</h2>
 
       {pageTitle && <h3>{pageTitle}</h3>}
 
