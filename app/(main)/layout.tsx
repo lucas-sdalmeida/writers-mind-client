6aa96f502d64760efbd8ccbeb5974c8794c4ef7f
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2025-05-23 22:16:35 -0300

    feat: change bg base global color and clear main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,10 @@
 import MainHeader from './components/MainHeader'
-import SideBar from './components/SideBar'
 
 export default function MainLayout({ children }: Readonly<Props>) {
   return (
-    <div className='w-dvw min-h-dvh grid grid-cols-[13rem_1fr] grid-rows-[auto_1fr] relative'>
-      <MainHeader className='col-span-full row-start-1 relative z-[60]' />
-
-      <SideBar className='col-start-1 row-start-2 relative z-[90]' />
-
-      <main className='col-start-2 row-start-2 relative z-30'>{children}</main>
+    <div className='w-dvw min-h-dvh flex flex-col'>
+      <MainHeader />
+      <main className='flex-1'>{children}</main>
     </div>
   )
 }
