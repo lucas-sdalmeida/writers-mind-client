212929d75a299b42343cd1b57bd05f2285ef7f6a
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2025-05-24 12:21:41 -0300

    refactor: improve header shadow

--- app/(main)/components/MainHeader/MainHeader.tsx
+++ app/(main)/components/MainHeader/MainHeader.tsx
@@ -5,7 +5,7 @@ const mrDafoe = Mr_Dafoe({ weight: '400', subsets: ['latin'] })
 
 export default function MainHeader() {
   return (
-    <header className='w-full h-12 py-1 px-4 shadow-md shadow-gray-400 flex justify-between items-center'>
+    <header className='w-full h-12 py-1 px-4 shadow-sm shadow-gray-300 flex justify-between items-center'>
       <h2 className={`${mrDafoe.className} text-black text-xl`}>Writer&apos;s Mind</h2>
       <AuthorInfo orientation={Orientation.LEFT} hidePseudonym={true} />
     </header>
