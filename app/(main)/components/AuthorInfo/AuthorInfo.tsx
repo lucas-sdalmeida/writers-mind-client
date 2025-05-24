f7bf402f2f358db9bce95abbc40e9c6855f09f88
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2025-05-23 22:19:53 -0300

    feat: improve author info layout and remove unnecessary header className

--- app/(main)/components/AuthorInfo/AuthorInfo.tsx
+++ app/(main)/components/AuthorInfo/AuthorInfo.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 import ProfilePicture from '../ProfilePicture'
 
 export default async function AuthorInfo({
-  orientation,
-}: Readonly<{ orientation: Orientation }>) {
+  orientation, hidePseudonym
+}: Readonly<{ orientation: Orientation, hidePseudonym?: boolean }>) {
   const orientationClass =
     orientation == Orientation.LEFT
       ? 'flex-row justify-start'
@@ -17,10 +17,10 @@ export default async function AuthorInfo({
         href='/author/1'
         className={`flex items-center gap-2 ${orientationClass} text-black decoration-0`}
       >
-        <UsernameAndPseudonym user={user} className={orientationClass} />
+        <UsernameAndPseudonym user={user} className={orientationClass} hidePseudonym={hidePseudonym} />
         <ProfilePicture
           radius={36}
-          src={user.pictureUrl || 'https://picsum.photos/seed/profile/40'}
+          src={user.pictureUrl || 'https://picsum.photos/seed/me/40'}
         />
       </Link>
     </div>
@@ -43,16 +43,18 @@ function findCurrentUser() {
 function UsernameAndPseudonym({
   user,
   className,
+  hidePseudonym,
 }: Readonly<UsernameAndPseudonymProps>) {
   return (
     <div className={`py-1 flex flex-col justify-center ${className}`}>
       <p className='text-sm font-bold'>{user.username}</p>
-      {user.pseudonym && <p className='text-[.65rem] font-bold text-[#444444]'>{user.pseudonym}</p>}
+      {user.pseudonym && !hidePseudonym && <p className='text-[.65rem] font-bold text-[#444444]'>{user.pseudonym}</p>}
     </div>
   )
 }
 
 type UsernameAndPseudonymProps = {
-  user: { username: string; pseudonym?: string }
-  className: string
+  user: { username: string; pseudonym?: string },
+  className: string,
+  hidePseudonym?: boolean,
 }
--- app/(main)/components/MainHeader/MainHeader.tsx
+++ app/(main)/components/MainHeader/MainHeader.tsx
@@ -3,16 +3,11 @@ import AuthorInfo, { Orientation } from '../AuthorInfo'
 
 const mrDafoe = Mr_Dafoe({ weight: '400', subsets: ['latin'] })
 
-export default function MainHeader({ className, pageTitle }: Props) {
+export default function MainHeader() {
   return (
-    <header className={`${className} w-full h-12 py-1 px-4 shadow-md shadow-gray-400 bg-[#f6f6f6] flex justify-between items-center`}>
-      <h2 className={`${mrDafoe.className} text-xl`}>Writer&apos;s Mind</h2>
-
-      {pageTitle && <h3>{pageTitle}</h3>}
-
-      <AuthorInfo orientation={Orientation.LEFT} />
+    <header className='w-full h-12 py-1 px-4 shadow-md shadow-gray-400 flex justify-between items-center'>
+      <h2 className={`${mrDafoe.className} text-black text-xl`}>Writer&apos;s Mind</h2>
+      <AuthorInfo orientation={Orientation.LEFT} hidePseudonym={true} />
     </header>
   )
 }
-
-type Props = { className?: string; pageTitle?: string }
