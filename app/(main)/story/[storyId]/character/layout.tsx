c1507dfaa74489ed8c882831f5c9b1afc88a3496
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2025-05-28 08:46:56 -0300

    feat: extract common character layout to layout.tsx

---
+++ app/(main)/story/[storyId]/character/layout.tsx
@@ -0,0 +1,35 @@
+import { Search, LibraryBig, GitCommitHorizontal, UsersRound, Waypoints, Plus } from "lucide-react" 
+import SideBar, { LinkOption } from "@/app/(main)/components/SideBar"
+
+export default async function CharactersLayout({ params, children }: Readonly<Props>) {
+    const storyId = await params
+
+    return (        
+        <div className='w-full h-full px-8 pt-10 pb-6 grid grid-cols-12 grid-rows-1'>
+            <SideBar
+                className='col-span-2'
+                header={
+                <>
+                    <Search size={16} />
+                    <div className='py-1 text-gray-400 text-sm'>Pesquisar</div>
+                </>
+                }
+                selectedIndex={2}
+            >
+                <LinkOption href='/story' icon={<LibraryBig size={16} />} >Salão das Histórias</LinkOption>
+                <LinkOption href='#' icon={<GitCommitHorizontal size={16} />} >Linha do Tempo</LinkOption>
+                <LinkOption href='#' icon={<UsersRound size={16} />} >Salão dos Heróis</LinkOption>
+                <LinkOption href='#' icon={<Waypoints size={16} />} >Mapa de Ideias</LinkOption>
+            </SideBar>
+
+            <div className="col-start-3 col-span-10 w-full h-full">
+                { children }
+            </div>
+        </div>
+    )
+}
+
+type Props = { 
+    params: Promise<{ storyId: string }>, 
+    children: React.ReactNode,
+}
