5a6d0e5ebc47a0412faf90577859467133188bc6
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2025-05-28 13:32:15 -0300

    feat: improve character layout

--- app/(main)/story/[storyId]/character/layout.tsx
+++ app/(main)/story/[storyId]/character/layout.tsx
@@ -2,8 +2,8 @@ import { Search, LibraryBig, GitCommitHorizontal, UsersRound, Waypoints, Plus } 
 import SideBar, { LinkOption } from "@/app/(main)/components/SideBar"
 
 export default async function CharactersLayout({ params, children }: Readonly<Props>) {
-    const storyId = await params
-
+    const { storyId } = await params
+    
     return (        
         <div className='w-full h-full px-8 pt-10 pb-6 grid grid-cols-12 grid-rows-1'>
             <SideBar
@@ -17,9 +17,9 @@ export default async function CharactersLayout({ params, children }: Readonly<Pr
                 selectedIndex={2}
             >
                 <LinkOption href='/story' icon={<LibraryBig size={16} />} >Salão das Histórias</LinkOption>
-                <LinkOption href='#' icon={<GitCommitHorizontal size={16} />} >Linha do Tempo</LinkOption>
-                <LinkOption href='#' icon={<UsersRound size={16} />} >Salão dos Heróis</LinkOption>
-                <LinkOption href='#' icon={<Waypoints size={16} />} >Mapa de Ideias</LinkOption>
+                <LinkOption href={`/story/${storyId}/time-line`} icon={<GitCommitHorizontal size={16} />} >Linha do Tempo</LinkOption>
+                <LinkOption href={`/story/${storyId}/character`} icon={<UsersRound size={16} />} >Salão dos Heróis</LinkOption>
+                <LinkOption href={`/story/${storyId}/idea`} icon={<Waypoints size={16} />} >Mapa de Ideias</LinkOption>
             </SideBar>
 
             <div className="col-start-3 col-span-10 w-full h-full">
