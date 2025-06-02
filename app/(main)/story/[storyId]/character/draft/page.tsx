75bbd3210deb6c009f9e3b7301d43a87248ab355
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2025-06-02 11:21:04 -0300

    refactor: replace raw inputs and button by the components

--- app/(main)/story/[storyId]/character/draft/page.tsx
+++ app/(main)/story/[storyId]/character/draft/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { Inter } from 'next/font/google'
 import { Image as ImageIcon, PenSquare } from 'lucide-react'
 import { useState } from 'react'
 
-const inter = Inter({ weight: '600', subsets: ['latin'] })
+import { EditableText } from '@/app/(main)/components/InputField'
+import { ConfirmButton, DangerButton } from '@/app/(main)/components/Button'
 
 export default function CharacterDraftPage() {
   const [character, setCharacter] = useState({} as CharacterDraft)
@@ -13,29 +13,18 @@ export default function CharacterDraftPage() {
     <form className='w-full h-full flex gap-3'>
       <div className='flex-1 px-8 flex flex-col gap-3'>
         <div className='w-full px-1 border-b-[1px] border-b-[#10c3e2]'>
-          <label className='w-full flex items-start gap-3'>
-            <input
-              className={`${inter.className} bg-transparent outline-none placeholder-gray-400`}
-              value={character.name ?? ''}
-              placeholder='Me chamo...'
-              onChange={(e) =>
-                setCharacter({ ...character, name: e.target.value })
-              }
-            />
-            <PenSquare size={14} />
-          </label>
+          <EditableText
+            placeholder='Me chamo...'
+            value={character.name ?? ''}
+            onChange={(v) => setCharacter({ ...character, name: v })}
+          />
         </div>
 
         <div className='flex-1 w-full h-full'></div>
 
         <div className='w-full p-2 flex justify-end items-center gap-3'>
-          <button className='px-3 py-1 rounded-xl bg-[#10c3e2] hover:bg-cyan-600 text-white hover:underline'>
-            Salvar
-          </button>
-
-          <button className='px-3 py-1 rounded-xl bg-[#e21010] hover:bg-[#a21010] text-white hover:underline'>
-            Cancelar
-          </button>
+          <ConfirmButton>Salvar</ConfirmButton>
+          <DangerButton>Cancelar</DangerButton>
         </div>
       </div>
 
